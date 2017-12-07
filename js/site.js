69ce6881c8e4c577eb92b0e98e5af3bd01aa14e0
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-12-06 20:19:53 -0800

    Added jquery for navigation

--- js/site.js
+++ js/site.js
@@ -1,5 +1,13 @@
 (function($) {
 
-    
+    $("#nav-overlay").hide();
+
+    $("#nav-button").click(function(){
+        $("#nav-overlay").show();
+    });
+
+    $("#nav-close-button").click(function(){
+        $("#nav-overlay").hide();
+    });
 
 }(jQuery));
